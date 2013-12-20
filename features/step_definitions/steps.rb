f82c04d2271e4704fb115dd73d0fa9622f745127
Thomas Ochman <thomas@nocebo.se>
2013-12-20 19:46:21 +0100

    Testing index.html and running a google search with Capybara to show the functionality

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,80 @@
+Given(/^I am on the home page$/) do
+  visit 'index.html'
+  page.source.should have_selector('html')
+  page.source.should have_selector('body')
+end
+
+Given(/^I am on the google site$/) do
+  visit 'http://www.google.com'
+end
+
+Given(/^I search for "(.*?)"$/) do |query|
+  fill_in('q', :with => query)
+  find('input[name="btnG"]').click
+  #click_link_or_button 'btnG'
+end
+
+Given(/^I am on the "(.*?)" page$/) do |page|
+  visit '/#{page}'
+end
+
+Then(/^I should see "(.*?)"$/) do |string|
+  page.should have_content string
+end
+
+Then(/^I should see a navigation header$/) do
+  page.should have_css('section#header')
+end
+
+Then(/^I should see a main content area$/) do
+  page.should have_css('section#main')
+end
+
+Then(/^I should see a footer area$/) do
+  page.should have_css('section#footer')
+end
+
+Then /^show me the page$/ do
+  save_and_open_page
+end
+
+Then(/^I should see a link to "(.*?)"$/) do |link|
+  page.should(have_css("a", :text => link))
+end
+
+Then /^(?:|I )should see the "([^\"]*)" link$/ do |link|
+  page.should(have_css("a", :text => link))
+end
+
+Then /^(?:|I )should not see the "([^\"]*)" link$/ do |link|
+  page.should_not(have_css("a", :text => link))
+end
+
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+When /^(?:|I )click "([^"]*)"$/ do |button|
+  click_link_or_button(button)
+end
+
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
+  fill_in(field, :with => value)
+end
+
+Then(/^the page should be titled "(.*?)"$/) do |title|
+  page.should have_selector("title", title)
+end
+
+Then /^I should see a link that points to "([^"]*)"$/ do |href_destination|
+  page.should have_xpath("//a[@href='#{href_destination}']")
+end
+
--- features/webbapp.feature
+++ features/webbapp.feature
@@ -4,7 +4,14 @@ Feature: As a developer,
 
 Scenario: Load the site
   Given I am on the home page
-  Then I should see "Hello World"
+  Then I should see a navigation header
+  And I should see a main content area
+  And I should see a footer area
 
+  Scenario: Search with www.google.com
+    Given I am on the google site
+    And I search for "Sweden"
+    Then show me the page
+    Then I should see a link to "Sweden - Wikipedia, the free encyclopedia"
 
 
