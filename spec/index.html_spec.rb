4bdbcb8efba35e0dd84ffa4fc45eb36e06f9bba6
Thomas Ochman <thomas@nocebo.se>
2014-10-19 14:24:41 +0000

    refactored view spec for 'index.html'

--- spec/index.html_spec.rb
+++ spec/index.html_spec.rb
@@ -9,4 +9,26 @@ describe '../public/index.html' do
   it {expect(page).to have_css 'section#main'}
   it {expect(page).to have_css 'section#footer'}
   
+  it 'should have project name in header' do
+    within 'section#header' do
+      expect(page).to have_css 'h1', text: 'Static1'
+    end
+  end
+  it 'should have p tags in main' do 
+    within 'section#main' do    
+      expect(page).to have_css 'p', count: 3
+    end
+  end
+  
+  context 'should have content in the right p tag' do
+    before(:each) do
+      within 'section#main' do
+        @paragraph = page.all('p')
+      end
+    end
+    
+    it { expect(@paragraph[0]).to have_text 'My content - Static1 Nitrous is awesome' }  
+    it { expect(@paragraph[1]).to have_text 'Lorem ipsum' }
+       
+  end
 end
